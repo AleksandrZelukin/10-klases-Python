f14a811f37515c0f0a23596d1d30dc68049c456b
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-02-05 15:36:05 +0200

    Update apps

--- csdd.py
+++ csdd.py
@@ -17,20 +17,30 @@ cur.execute("""CREATE TABLE IF NOT EXISTS auto(
     ID_ipasnieks INT,
     FOREIGN KEY (ID_ipasnieks) REFERENCES ipasnieks(ID_ipasnieks))""")
 
-i = int(input("Ipašnieka numurs: "))
-v = input("Ipašnieka vārds: ")
-t = input("Ipašnieka talrunis: ")
-print("Aizpidiet automobilus informāciju un pievienojiet īpašnieka numurs")
-a = input("Automobila numurs: ")
-mar = input("Auto marka: ")
-mod = input("Auto modelis: ")
-g = input("Auto gads: ")
-ia = int(input("Ipašnieka auto: "))
-ipasn = (i,v,t)
-aut = (a,mar,mod,g,ia)
+# i = int(input("Ipašnieka numurs: "))
+# v = input("Ipašnieka vārds: ")
+# t = input("Ipašnieka talrunis: ")
+# print("Aizpidiet automobilus informāciju un pievienojiet īpašnieka numurs")
+# a = input("Automobila numurs: ")
+# mar = input("Auto marka: ")
+# mod = input("Auto modelis: ")
+# g = input("Auto gads: ")
+# ia = int(input("Ipašnieka auto: "))
+# ipasn = (i,v,t)
+# aut = (a,mar,mod,g,ia)
 
-cur.execute("""INSERT INTO ipasnieks(ID_ipasnieks,vards,talrunis)VALUES(?,?,?)""",ipasn)
-cur.execute("""INSERT INTO auto(ID_auto,marka,model,gads,ID_ipasnieks)VALUES(?,?,?,?,?)""",aut)
+# cur.execute("""INSERT INTO ipasnieks(ID_ipasnieks,vards,talrunis)VALUES(?,?,?)""",ipasn)
+# cur.execute("""INSERT INTO auto(ID_auto,marka,model,gads,ID_ipasnieks)VALUES(?,?,?,?,?)""",aut)
+# cur.execute("""SELECT vards,marka,model 
+#             FROM ipasnieks,auto 
+#             WHERE ipasnieks.ID_ipasnieks = auto.ID_ipasnieks""")
 
+cur.execute("""SELECT vards,marka,model 
+            FROM ipasnieks,auto 
+            WHERE ipasnieks.vards = "Valdis" 
+            AND ipasnieks.ID_ipasnieks = auto.ID_ipasnieks""")
+kopa = cur.fetchall()
+for i in kopa:
+    print(i)
 con.commit()
 cur.close()
