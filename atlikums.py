8eab4adea38a254f227334697d48be463d466589
Aleksandr Zelukin <92846541+AleksandrZelukin@users.noreply.github.com>
2025-03-05 19:29:12 +0200

    Update apps

--- atlikums.py
+++ atlikums.py
@@ -10,10 +10,24 @@ e = (atlikums-a*50-b*20-c*10-d*5)//2
 f = (atlikums-a*50-b*20-c*10-d*5-e*2)//1
 
 print(f"""Lai izdot atlikumu {atlikums} centu, nepiecešams 
-      {a} monetas pa 50, 
-      {b} monetas pa 20, 
-      {c} monetas pa 10, 
-      {d} monetas pa 5,
-      {e} monetas pa 2 un 
-      {f} monetas pa 1 centu""")
+      {a} monetas pa {kassa[0]}, 
+      {b} monetas pa {kassa[1]}, 
+      {c} monetas pa {kassa[2]}, 
+      {d} monetas pa {kassa[3]},
+      {e} monetas pa {kassa[4]} un 
+      {f} monetas pa {kassa[5]} centu""")
 
+atlikums = int(input("Ievadiet atlikumu summu: "))
+
+kassa = (50, 20, 10, 5, 2, 1)
+
+monetas = []
+
+for j in kassa:
+    count = atlikums // j
+    monetas.append(count)
+    atlikums %= j
+
+print(f"Lai izdot atlikumu {atlikums} centu, nepieciešams:")
+for i, j in enumerate(kassa):
+    print(f"{monetas[i]} monētas pa {j} centiem")
