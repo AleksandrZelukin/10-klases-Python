ec2c321f584b8c83ba1bc440c6d9eed7ca0b4380
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-03-27 13:39:32 +0200

    Update apps

--- darbs_ar_failiem_1.py
+++ darbs_ar_failiem_1.py
@@ -2,8 +2,22 @@ x = int(input("Ievadi skaitli x: "))
 y = int(input("Ievadi skaitli y: "))
 z = x + y 
 print(z)
+z = x + y
 
-a = open("saraksts.txt", "a",encoding="utf-8")
+a = open("saraksts.txt", "w",encoding="utf-8")
 
 print(z,file=a)
+a.close()
+
+a = open("saraksts.txt", "r",encoding="utf-8")
+
+b = int(a.read())
+s = int(input("Ievadi skaitli s: "))
+s = s + b
+
+# print(a.read())
+
+print(s)
+a = open("saraksts.txt", "a",encoding="utf-8")
+print(s,file=a)
 a.close()
