ff09f9a8460f2f2c01ee866c5860cb831cd50c8c
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-04-02 10:48:29 +0300

    Update apps

--- darbs_ar_failiem4.py
+++ darbs_ar_failiem4.py
@@ -1,15 +1,18 @@
 import csv
-print(dir(csv))
 # Izveido CSV failu ar datiem un izdrukā to saturu
 # Atver failu lasīšanai
 with open('datne.csv', 'r', encoding='utf-8') as csvfile:
     # Izveido CSV lasītāju
-    csvreader = csv.reader(csvfile, delimiter=';')
+    csvreader = csv.reader(csvfile, delimiter=',')
+    # Izdrukā pirmo rindu (saraksta nosaukumus)
 
     # Izdrukā katru rindu failā
     for row in csvreader:
-        print(row)
-
+        # if row[0] == 'Anna':
+            # print(f'{row[0]} {row[1]} {row[2]}')
+        # print(row)
+        print(f'{row[0]} {row[1]} {row[2]}')
+# Izveido CSV failu ar datiem
 saraksts = [{'Vārds': 'Jānis', 'Uzvārds': 'Bērziņš', 'Vecums': 25},
            {'Vārds': 'Anna', 'Uzvārds': 'Kalniņa', 'Vecums': 30},
            {'Vārds': 'Pēteris', 'Uzvārds': 'Ozols', 'Vecums': 28}]
@@ -17,7 +20,7 @@ saraksts = [{'Vārds': 'Jānis', 'Uzvārds': 'Bērziņš', 'Vecums': 25},
 # Atver failu rakstīšanai
 with open('datne.csv', 'w', encoding='utf-8',newline='') as csvfile:
     # Izveido CSV rakstītāju
-    fliednames = ['Vārds', 'Uzvārds', 'Vecums']
-    csvwriter = csv.DictWriter(csvfile, fieldnames=fliednames)
+    flied_names = ['Vārds', 'Uzvārds', 'Vecums']
+    csvwriter = csv.DictWriter(csvfile, fieldnames=flied_names)
     csvwriter.writeheader()
     csvwriter.writerows(saraksts)
