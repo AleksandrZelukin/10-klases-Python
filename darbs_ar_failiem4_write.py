e6e25369b655b26611ff1b634b2c935caf7509b1
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-04-02 10:52:10 +0300

    Update apps

--- darbs_ar_failiem4_write.py
+++ darbs_ar_failiem4_write.py
@@ -1,17 +1,4 @@
 import csv
-# Izveido CSV failu ar datiem un izdrukā to saturu
-# Atver failu lasīšanai
-with open('datne.csv', 'r', encoding='utf-8') as csvfile:
-    # Izveido CSV lasītāju
-    csvreader = csv.reader(csvfile, delimiter=',')
-    # Izdrukā pirmo rindu (saraksta nosaukumus)
-
-    # Izdrukā katru rindu failā
-    for row in csvreader:
-        # if row[0] == 'Anna':
-            # print(f'{row[0]} {row[1]} {row[2]}')
-        # print(row)
-        print(f'{row[0]} {row[1]} {row[2]}')
 # Izveido CSV failu ar datiem
 saraksts = [{'Vārds': 'Jānis', 'Uzvārds': 'Bērziņš', 'Vecums': 25},
            {'Vārds': 'Anna', 'Uzvārds': 'Kalniņa', 'Vecums': 30},
