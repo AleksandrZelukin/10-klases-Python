57c77d1c2e817963bf3134f6fef40a61ba1ee5da
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-04-30 11:27:28 +0300

    Update apps

--- funkcija_generators.py
+++ funkcija_generators.py
@@ -1,7 +1,15 @@
-a = ["Pirmdiena","Otrdiena","Trešdiena",
-     "Ceturtdiena","Piektdiena",
-     "Sestdiena","Svētdiena"]
+# https://youtu.be/PrloUblCsFI?si=SNw6SKHkRvaZv9QY
+def nedelas(s=0):
+    a = ["Pirmdiena","Otrdiena","Trešdiena",
+        "Ceturtdiena","Piektdiena",
+        "Sestdiena","Svētdiena"]
 
+    while True:
+        yield a[s]
+        s = (s + 1) % len(a)
 
-for i in a:
-    print(i)
+aa = input("Ievadi nodēļas dienas numuru (0-6): ")
+bb = input("Ievadi nodēļas dienas daudzumu: ")
+nedelas_generator = nedelas(int(aa))
+for i in range(int(bb)):
+    print(next(nedelas_generator))
---
+++ funkcija_lambda.py
@@ -0,0 +1,8 @@
+def parasta_funkcija(a, b):
+    return a + b
+
+lambda_funkcija = lambda a, b: a + b
+
+
+print(parasta_funkcija(2, 3))
+print(lambda_funkcija(2, 3))
