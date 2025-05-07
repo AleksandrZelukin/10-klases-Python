ca131cc51fe57894bd6b7c2655f82c9127d01f7b
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-05-07 10:29:16 +0300

    Update apps

--- funkcijas.py
+++ funkcijas.py
@@ -1,7 +1,10 @@
-def trijsturas_laukums(a,h):
-    s = (a*h)/2
-    print("trijstura laukums: ",s)
+# def trijsturas_laukums(a,h):
+#     s = (a*h)/2
+#     print("trijstura laukums: ",s)
 
 x = int(input("Pamats: "))
 y = int(input("Augstums: "))
-a = trijsturas_laukums(x,y)
+# a = trijsturas_laukums(x,y)
+
+trijsturas_laukums= lambda a,h: (a*h)/2
+print("trijstura laukums: ",trijsturas_laukums(x,y))
