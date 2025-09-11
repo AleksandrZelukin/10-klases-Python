3999a7c3fc4e324bf051a4d1b675c87357f0c837
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-09-11 13:48:50 +0300

    Create apps

---
+++ zarosana.py
@@ -0,0 +1,13 @@
+summa = int(input("Ievadi summu: "))
+cena = float(input("Ievadi cenu: "))
+daudzums = int(input("Ievadi daudzumu: "))
+real_daudzums = int(summa//cena)
+truks = round(cena*daudzums - summa, 2)
+atlikums = round(summa - int(summa//cena)*cena, 2)
+if summa >= cena*daudzums:
+    atlikums = round(summa - cena*daudzums, 2)
+    print(f"Atlikums bus: {atlikums} eiro")
+else:
+    print ("Nav pietiekami naudas")
+    print (f"Trukst: {truks} eiro")
+    print(f"Tu vari nopirkt: {real_daudzums} kg. ābolus un atlikums bus: {atlikums} eiro")
