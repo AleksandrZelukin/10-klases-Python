ac96546083ae00ae7d50e630fc08709651f79f19
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-09-12 13:30:19 +0300

    Update apps

--- zarosanas2.py
+++ zarosanas2.py
@@ -6,4 +6,10 @@ if a >= b and a >= c and b >= c:
 elif b >= c and b >= a and c >= a:
     print(b, c, a)
 elif c >= a and c >= b and a >= b:
-    print(c, a, b) 
+    print(c, a, b)
+elif a >= c and a >= b and c >= b:
+    print(a, c, b)  
+elif b >= a and b >= c and a >= c:
+    print(b, a, c)
+elif c >= b and c >= a and b >= a:
+    print(c, b, a)
