4f10e9e3ffe255b289e29f33697ece2dd671edd3
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-10-03 12:42:56 +0300

    Update apps

--- Vardnica_1.py
+++ Vardnica_1.py
@@ -1,9 +1,39 @@
 phonebook = dict() #dict
+print("""
+      Ko vēlies darīt? 
+      pievienot ierakstu (1) 
+      meklēt ierakstu (2) 
+      beigt (3)?""")
 
-while True:
-    vards = input("Vārds: ")
-    if vards == "stop":
-        break    
-    talr = input("Tālrunis: ")
-    phonebook[vards] = talr
-print(phonebook)
+if input() == "1":
+     print("Ievadi vārdus un telefona numurus")
+     while True:
+         vards = input("Vārds: ")
+         talr = input("Tālrunis: ")
+         phonebook[vards] = talr
+         print("""
+      Ko vēlies darīt? 
+      pievienot ierakstu (1) 
+      meklēt ierakstu (2) 
+      beigt (3)?""")
+         if input() == "2":
+             break
+         if input() == "1":
+             continue   
+     print(phonebook)
+
+if input() == "2":
+    vards = input("Ievadi vārdu, kuru meklēt: ")
+    if vards in phonebook:
+        print(f"Vārds: {vards}, Tālrunis: {phonebook[vards]}")
+    else:
+        print("Šis vārds nav atrasts.")
+if input() == "3":
+    print("Ievadi vārdus un telefona numurus. Lai beigtu, ieraksti 'stop' kā vārdu.")
+    while True:
+        vards = input("Vārds: ")
+        if vards == "stop":
+            break
+        talr = input("Tālrunis: ")
+        phonebook[vards] = talr
+    print(phonebook)
