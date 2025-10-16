39e294aaf67823e5ec70ccfefd6517d2ae5cca08
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-10-16 13:34:28 +0300

    Update apps

--- vardnicas_1.py
+++ vardnicas_1.py
@@ -3,13 +3,30 @@ a = {"Rīga": "Latvija", "Tallinn": "Igaunija",
      "Daugavpils": "Latvija", "Tartu": "Igaunija", 
      "Kaunas": "Lietuva", "Turku": "Somija"}
 
-while True:
-    b = input("Pilsēta: ")
-    c = input("valsts: ")
-    a[b]=c
-    if b == "stop":
-        break
+# while True:
+#     b = input("Pilsēta: ")
+#     c = input("valsts: ")
+#     a[b]=c
+#     if b == "stop":
+#         break
 
+# print("Pilsēta - valsts")
 
-for i in a:
-    print (i,a[i])
+# for i in a:
+#     print (i,"-",a[i])
+    
+print("Pilsēta: ")
+pilseta = input()
+if pilseta in a:
+    print("Valsts:", a[pilseta])
+    print("Ievadiet jaunu pilsētu vai 'stop', lai pabeigtu:")
+    while True:
+        jauna_pilseta = input()
+        if jauna_pilseta == "stop":
+            break
+        if jauna_pilseta in a:
+            print("Valsts:", a[jauna_pilseta])
+        else:
+            print("Šī pilsēta nav datu bāzē.")
+            
+            
