b8bb43e9f9127c6e22d75ee7837daefcdefca3bc
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-11-28 13:37:09 +0200

    Update apps

---
+++ darbs_ar_failiem_talruni.py
@@ -0,0 +1,8 @@
+while True:
+    vards = input("Vārds: ")
+    if vards == "stop":
+        break
+    else:
+        talrunis = input("Talruņa numurs: ")
+        d = open("darbs_ar_failiem_talruni.txt","a",encoding="utf-8")
+        print(f"{vards}, {talrunis}",file=d)
