9eb927ac07a418e0cd787bd01621056cdd8e42ee
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-12-04 14:12:04 +0200

    Update apps

--- atlikums.py
+++ atlikums.py
@@ -9,12 +9,18 @@ d = (atlikums-a*50-b*20-c*10)//5
 e = (atlikums-a*50-b*20-c*10-d*5)//2
 f = (atlikums-a*50-b*20-c*10-d*5-e*2)//1
 
-print(f"""Lai izdot atlikumu {atlikums} centu, nepiecešams 
-      {a} monetas pa {kassa[0]}, 
-      {b} monetas pa {kassa[1]}, 
-      {c} monetas pa {kassa[2]}, 
-      {d} monetas pa {kassa[3]},
-      {e} monetas pa {kassa[4]} un 
-      {f} monetas pa {kassa[5]} centu""")
+print(f"Lai izdot atlikumu {atlikums} centu, nepiecešams") 
+if a != 0:
+      print(f"{a} monetas pa {kassa[0]}")
+if b != 0:
+      print(f"{b} monetas pa {kassa[1]}")
+if c != 0:
+      print(f"{c} monetas pa {kassa[2]}")
+if d != 0:
+      print(f"{d} monetas pa {kassa[3]}")
+if e != 0:
+      print(f"{e} monetas pa {kassa[4]}")
+if f != 0:
+      print(f"{f} monetas pa {kassa[5]}")
 
 
