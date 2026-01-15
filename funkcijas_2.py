27390eef369d5ccfc82815e09559bf8d8001457c
Aleksandr Zelukin <alexandr44@inbox.lv>
2026-01-15 14:14:48 +0200

    Create apps

--- funkcijas_2.py
+++ funkcijas_2.py
@@ -3,8 +3,8 @@ b = 6
 c = 12
 d = 8
 
-def pirma_funkcija (x=0,y=6):
-    return x+y
-
+# def pirma_funkcija (x=0,y=6):
+#     return x+y
+pirma_funkcija = lambda x=0,y=6: x+y
 rezultats = pirma_funkcija(a,d)
 print(rezultats)
