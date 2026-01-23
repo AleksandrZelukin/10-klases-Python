f85816b883d8a2fa84220c0359981d2fc6859747
Aleksandr Zelukin <alexandr44@inbox.lv>
2026-01-23 11:12:35 +0200

    Update parbaudes_darbs.py

--- parbaudes_darbs.py
+++ parbaudes_darbs.py
@@ -6,20 +6,30 @@
 # - Raksti tikai nepieciešamo kodu.
 # - Funkciju nosaukumiem jāsakrīt ar uzdevumā dotajiem.
 # - Atbildes augšupielādē .py failu.
+# ======================================================
 # 1. uzdevums (10 punkti)
-# Izveidot funkciju skaitlu_summa(saraksts), kas atgriež visu skaitļu summu.
+# Izveidot funkciju skaitlu_summa(saraksts), 
+# kas atgriež visu skaitļu summu.
 # Piemērs:
 # saraksts = [3, 7, 2] → rezultāts: 12
+# ======================================================
 # 2. uzdevums (10 punkti)
-# Izveidot funkciju pozitivi_skaitli(saraksts), kas atgriež tikai pozitīvos skaitļus.
+# Izveidot funkciju pozitivi_skaitli(saraksts), 
+# kas atgriež tikai pozitīvos skaitļus.
 # Piemērs:
 # saraksts = [-2, 5, 0, 8, -1] → rezultāts: [5, 8]
+# ======================================================
 # 3. uzdevums (10 punkti)
 # Izveidot funkciju maksimalais(saraksts), neizmantojot max().
+# ======================================================
 # 4. uzdevums (10 punkti)
-# Ievadīt 5 skaitļus, saglabāt sarakstā un izvadīt summu, pozitīvos skaitļus un lielāko skaitli.
+# Ievadīt 5 skaitļus, saglabāt sarakstā un izvadīt summu, 
+# pozitīvos skaitļus un lielāko skaitli.
+# ======================================================
 # 5. uzdevums (10 punkti)
-# Izveidot funkciju videjais(saraksts), kas aprēķina vidējo aritmētisko.
+# Izveidot funkciju videjais(saraksts), 
+# kas aprēķina vidējo aritmētisko.
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Vērtēšanas kritēriji
 # Katram uzdevumam:
 # • 10 punkti – uzdevums izpildīts pareizi, funkcija darbojas.
@@ -27,5 +37,7 @@
 # • 4–6 punkti – daļēji pareizs risinājums.
 # • 1–3 punkti – mēģinājums, bet funkcija nestrādā.
 # • 0 punkti – nav risinājuma.
+
+
 
 
