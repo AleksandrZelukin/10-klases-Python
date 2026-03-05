db592a2c0401841ff9bc458c1e555831cbd3b6a4
Aleksandr Zelukin <92846541+AleksandrZelukin@users.noreply.github.com>
2026-03-05 18:42:46 +0200

    Update maja_10b.py

--- maja_10b.py
+++ maja_10b.py
@@ -13,13 +13,16 @@ def maja(x,y,a,c,d,b=90):
         left(b)
     end_fill()
 maja(-100,-380,200,krasa[6],4) # Siena
-maja(40,-50,40,krasa[5],4) 
-maja(40,-10,40,krasa[5],4)
-maja(40,30,40,krasa[5],4)
-maja(40,70,40,krasa[5],4)
+for i in (-50,-10,30,70):
+    maja(40,i,40,krasa[5],4)
+# maja(40,-50,40,krasa[5],4) 
+# maja(40,-10,40,krasa[5],4)
+# maja(40,30,40,krasa[5],4)
+# maja(40,70,40,krasa[5],4)
 maja(-150,-180,300,krasa[4],3,120)
 maja(0,-280,80,krasa[5],4)
-maja(-90,-380,70,krasa[3],4)
-maja(-90,-310,70,krasa[3],4)
+for k in (-380,-310):
+    maja(-90,k,70,krasa[3],4)
 maja(-15,-110,30,krasa[3],8,45)
+
 mainloop()
