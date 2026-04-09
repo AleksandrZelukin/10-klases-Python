280d3cba65dcb31765308d56d30d85464b49908b
Aleksandr Zelukin <alexandr44@inbox.lv>
2026-04-09 14:22:41 +0300

    Update logs_ka_tev_sauc.py

--- logs_ka_tev_sauc.py
+++ logs_ka_tev_sauc.py
@@ -6,7 +6,7 @@ logs.title("programma 'Ka tev sauc?'")
 
 def atbilde():
     atb["text"] = atb.get()
-    atb.config(text=("Sveiki",atb.get()))
+    lb2.config(text=("Sveiki",atb.get()))
 
 lb1 = Label(text = "Sveiki, draugs! Ka tevi sauc?", font=('Helvetika',24,'bold'))
 lb1.pack()
@@ -14,10 +14,10 @@ atb = Entry(font=('Helvetika',24,'bold'))
 atb.pack()
 btn = Button(text="Nospiež mani!",font=('Helvetika',24,'bold'),command=atbilde)
 btn.pack()
-lb2 = Label(text = "Sveiki")
+lb2 = Label(text = " ",font=('Helvetika',24,'bold'))
 lb2.pack()
-
-
+btn_izeja = Button(text="Iziet",font=('Helvetika',24,'bold'),command=logs.destroy)
+btn_izeja.pack(side=BOTTOM)
 
 
 logs.mainloop()
